3cae2bf373018d3ec84887e5497d1a721346dbf9
Harsh Ponia <harshponia9@gmail.com>
2022-08-16 20:09:33 +0530

    leetcode prectice

--- src/move_zeroes.java
+++ src/move_zeroes.java
@@ -1,3 +1,18 @@
+/*
+    Date : 16/aug/2022
+    Author : Harsh Ponia
+    Problem: 283. Move Zeroes
+    Difficulty: Easy
+    Description : Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the
+    non-zero elements.
+    Note :that you must do this in-place without making a copy of the array.
+    Example 1:
+        Input: nums = [0,1,0,3,12]
+        Output: [1,3,12,0,0]
+     */
+
+
+
 import java.util.Scanner;
 
 public class move_zeroes {
