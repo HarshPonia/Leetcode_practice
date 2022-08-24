dee8c50dc7b118d1e3dec69d765f21f9fad1b323
Harsh Ponia <harshponia9@gmail.com>
2022-08-24 17:03:39 +0530

    leetcode prectice

---
+++ src/Jump_game.java
@@ -0,0 +1,22 @@
+public class Jump_game {
+    public boolean canJump(int[] nums)
+    {
+        int n = nums.length;
+        if(n==1)
+        {
+            return true;
+        }
+        int max = 0;
+        for (int i = 0;i<n-1 && max>=i ; i++){
+            if(max<i+nums[i]){
+                max = i+nums[i];
+            }
+            if(max>=n-1)
+                return true;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+
+    }
+}
