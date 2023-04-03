baf312820c61cda48143709c56d774e19f5408de
Harsh Ponia <harshponia9@gmail.com>
2023-04-03 15:46:53 +0530

    POTD

---
+++ src/April_POTD/_0881_Boats_to_Save_People.java
@@ -0,0 +1,21 @@
+package April_POTD;
+
+public class _0881_Boats_to_Save_People {
+    public int numRescueBoats(int[] people, int limit) {
+        int count = 0, n = people.length;
+        Arrays.sort(people);
+        int p1 = 0, p2 = n-1;
+        while(p1<=p2){
+            count++;
+            if(people[p1]+people[p2]<=limit){
+                p1++;
+                p2--;
+            }
+            else{
+                p2--;
+            }
+        }
+        return count;
+    }
+}
+}
