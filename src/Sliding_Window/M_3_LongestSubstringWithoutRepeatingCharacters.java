644fa2825a1859b5167ed029cb1117a0fba7c699
Harsh Ponia <harshponia9@gmail.com>
2023-06-06 17:33:06 +0530

    Sliding window

---
+++ src/Sliding_Window/M_3_LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,17 @@
+package Sliding_Window;
+import java.util.*;
+public class M_3_LongestSubstringWithoutRepeatingCharacters {
+    public int lengthOfLongestSubstring(String s) {
+        int c =0;
+        int left = 0;
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(int  i =0;i<s.length();i++){
+            if(map.containsKey(s.charAt(i))){
+                left = Math.max(map.get(s.charAt(i))+1,left);
+            }
+            map.put(s.charAt(i),i);
+            c = Math.max(c, i-left+1);
+        }
+        return c;
+    }
+}
